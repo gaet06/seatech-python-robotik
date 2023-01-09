bf7ef2f1b3bf540d42ad4d56af90d0e5c036e41c
tatayoyoh <tatayoyoh@mymail>
2023-01-09 11:47:07 +0100

    ajout de l'exercice deux heritages

---
+++ exo1/exo2_starter_template.py
@@ -0,0 +1,67 @@
+from exo1_starter_template import Robot
+
+class Human():   
+    # Human class content here
+    __sexe = {'homme','femme','autre'}
+    __aliments = []
+
+    def __init__(self, sexe):
+
+        self.__sexe = sexe
+        
+    
+
+    def eat(self, aliment):
+
+        if type(aliment)==str :
+            self.__aliments.append(aliment)
+        elif type(aliment)==list:
+            self.__aliments += aliment
+        else:
+            print("j'ai pas graille frère ")
+
+
+    def digest(self):
+
+        if self.__aliments == []:
+            print('lache un croc avant de demander ça!') 
+            
+        else :
+            self.__aliments.clear() 
+
+        
+
+    @property 
+    def show_aliment(self):
+        print("j'ai mangé", self.__aliments)
+        
+    @property
+    def show_sexe(self):
+        print("je suis un(e)",self.__sexe)
+    
+
+class Cyborg(Robot, Human):   
+
+    def __init__(self, name, sexe):   
+        Robot.__init__(self, name)
+        Human.__init__(self, sexe)
+
+        
+
+
+cyborg = Cyborg('dagobert', 'Femme')
+cyborg.show_sexe
+
+cyborg.eat('pomme d eau fine')
+cyborg.show_aliment
+cyborg.show_name
+cyborg.digest()
+
+
+# print(cyborg.name, 'sexe', cyborg.sexe)
+# print('Charging battery...')
+# cyborg.charge()
+# cyborg.status()
+# cyborg.eat('banana')
+# cyborg.eat(['coca', 'chips'])
+# cyborg.digest()
